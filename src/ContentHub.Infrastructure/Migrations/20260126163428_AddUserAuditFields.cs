a9408bb0f9a95e3fe21585c5a1c878809a2ebf06
Lubabalo Buwa <lubabalobuwa64@gmail.com>
2026-01-26 18:38:21 +0200

    Added: migration for User Audit Fields i.e. CreatedAtUtc and LastLoginAtUtc

---
+++ src/ContentHub.Infrastructure/Migrations/20260126163428_AddUserAuditFields.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContentHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserAuditFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAtUtc",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginAtUtc",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAtUtc",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastLoginAtUtc",
+                table: "Users");
+        }
+    }
+}
