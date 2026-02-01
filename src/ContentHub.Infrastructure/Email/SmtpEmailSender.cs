4de7f0751ad5dfb69f54e85dd6e3f6a902a11e45
Lubabalo Buwa <lubabalobuwa64@gmail.com>
2026-02-01 20:25:36 +0200

    Add SMTP timeout and logging

--- src/ContentHub.Infrastructure/Email/SmtpEmailSender.cs
+++ src/ContentHub.Infrastructure/Email/SmtpEmailSender.cs
@@ -1,4 +1,5 @@
 using ContentHub.Application.Common.Interfaces;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Net;
 using System.Net.Mail;
@@ -9,30 +10,41 @@ namespace ContentHub.Infrastructure.Email
     public class SmtpEmailSender : IEmailSender
     {
         private readonly SmtpSettings _settings;
+        private readonly ILogger<SmtpEmailSender> _logger;
 
-        public SmtpEmailSender(IOptions<SmtpSettings> options)
+        public SmtpEmailSender(IOptions<SmtpSettings> options, ILogger<SmtpEmailSender> logger)
         {
             _settings = options.Value;
+            _logger = logger;
         }
 
         public async Task SendAsync(string to, string subject, string htmlBody)
         {
-            using var message = new MailMessage
+            try
             {
-                From = new MailAddress(_settings.FromAddress, _settings.FromName),
-                Subject = subject,
-                Body = htmlBody,
-                IsBodyHtml = true
-            };
-            message.To.Add(to);
+                using var message = new MailMessage
+                {
+                    From = new MailAddress(_settings.FromAddress, _settings.FromName),
+                    Subject = subject,
+                    Body = htmlBody,
+                    IsBodyHtml = true
+                };
+                message.To.Add(to);
 
-            using var client = new SmtpClient(_settings.Host, _settings.Port)
+                using var client = new SmtpClient(_settings.Host, _settings.Port)
+                {
+                    EnableSsl = _settings.UseStartTls,
+                    Credentials = new NetworkCredential(_settings.Username, _settings.Password),
+                    Timeout = 15000
+                };
+
+                await client.SendMailAsync(message);
+            }
+            catch (System.Exception ex)
             {
-                EnableSsl = _settings.UseStartTls,
-                Credentials = new NetworkCredential(_settings.Username, _settings.Password)
-            };
-
-            await client.SendMailAsync(message);
+                _logger.LogError(ex, "SMTP send failed. Host={Host} Port={Port} To={To}", _settings.Host, _settings.Port, to);
+                throw;
+            }
         }
     }
 }
